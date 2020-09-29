4265355ad0ac07c8723a0db8276b5c9340c6f2b0
Masamichi Hosoda <trueroad@trueroad.jp>
2020-09-29 21:09:27 +0900

    Fix to confirm to coding conventions
    コーディング規約に従い修正。
    flycheck, package-lint, flycheck-package で
    error, warning が一切出ないようにした。

--- w32-ime.el
+++ w32-ime.el
@@ -1,17 +1,77 @@
-;;;;; w32-ime.el ---- Meadow features for NTEmacs.
+;;; w32-ime.el --- Windows IME UI/UX controler
+
+;; Copyright (C) 1999      H.Miyashita
+;; Copyright (C) 2001-2003 MIYOSHI Masanori
+;; Copyright (C) 2004-2007 KOBAYASHI Yasuhiro
+;; Copyright (C) 2007-2009 KOBAYASHI Yasuhiro (NTEmacsJP)
+;; Copyright (C) 2013      ksugita (gnupack)
+;; Copyright (C) 2014      rzl24ozi
+;; Copyright (C) 2015-2020 TANE
+;; Copyright (C) 2020      Masamichi Hosoda
+
+;; Author:           H.Miyashita
+;;                   MIYOSHI Masanori
+;;                   KOBAYASHI Yasuhiro
+;;                   NTEmacsJP
+;;                   ksugita (gnupack)
+;;                   rzl24ozi
+;;                   TANE
+;;                   Masamichi Hosoda <trueroad@trueroad.jp>
+;; Maintainer:       Masamichi Hosoda <trueroad@trueroad.jp>
+;; URL:              https://github.com/trueroad/w32-ime.el
+;; Version:          20200929
+;; Package-Requires: ((emacs "24.3"))
+
+;; w32-ime.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;;   Author H.Miyashita
+;; w32-ime.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 ;;
-;;;;;
+;; You should have received a copy of the GNU General Public License
+;; along with tr-emacs-ime-module.
+;; If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; w32-ime.el, formerly known as "Meadow features for NTEmacs", was part of
+;; an IME patch for Emacs for Windows that allowed to input Japanese using
+;; the Windows IME (Input Method Editor).  On Emacs 26.2 or later, Japanese
+;; input using the IME is now possible even without the IME patch.  However,
+;; some of the features of the IME patch are not implemented in current
+;; Emacs, so it is possible to type Japanese on Emacs without the IME patch,
+;; but it is not convenient.
+;;
+;; w32-ime.el managed the upper layers of the IME patches and provided the
+;; convenient features, but it is not contained in current Emacs.  It has
+;; functions such as displaying the on/off state of the IME in the mode line
+;; as UI/UX features.  It also has hooks to call when the IME state is
+;; changed.  With these hooks, you can change the color and shape of the
+;; cursor depending on the IME ON/OFF status to be visually known to the
+;; IME state.
+
+;; To use w32-ime.el, add the following code to your init.el or .emacs
+;;
+;;   (setq default-input-method "W32-IME")
+;;   (w32-ime-initialize)
+
+;;; Code:
 
 (defgroup W32-IME nil
   "w32-ime"
   :group 'emacs)
 
-(defvar w32-last-selection nil
+(define-obsolete-variable-alias 'w32-last-selection
+  'w32-ime-last-selection "2020")
+
+(defvar w32-ime-last-selection nil
   "It is stored the last data from Emacs.")
 
-;----------
+;; ----------
 
 (defvar w32-ime-on-hook nil
   "Functions to eval when IME is turned on at least.
@@ -24,7 +84,7 @@ Even if IME state is not changed, these functiona are maybe called.")
 (defvar w32-ime-show-mode-line t
   "When t, mode line indicates IME state.")
 (defvar w32-ime-mode-line-state-indicator "[O]"
-  "This is shown at the mode line. It is regarded as state of ime.")
+  "This is shown at the mode line.  It is regarded as state of ime.")
 (make-variable-buffer-local 'w32-ime-mode-line-state-indicator)
 (put 'w32-ime-mode-line-state-indicator 'permanent-local t)
 (defvar w32-ime-mode-line-state-indicator-list '("-" "[|]" "[O]")
@@ -52,11 +112,16 @@ Even if IME state is not changed, these functiona are maybe called.")
 (global-set-key [kanji] 'ignore)
 (global-set-key [compend] 'ignore)
 
-(defun wrap-function-to-control-ime
+(define-obsolete-function-alias 'wrap-function-to-control-ime
+  #'w32-ime-wrap-function-to-control-ime "2020")
+
+(defun w32-ime-wrap-function-to-control-ime
   (function interactive-p interactive-arg &optional suffix)
   "Wrap FUNCTION, and IME control is enabled when FUNCTION is called.
+If INTERACTIVE-P is non-nil, FUNCTION is handled as interactive and uses
+INTERACTIVE-ARG as its arguments.
 An original function is saved to FUNCTION-SUFFIX when suffix is string.
-If SUFFIX is nil, \"-original\" is added. "
+If SUFFIX is nil, \"-original\" is added."
   (let ((original-function
 	 (intern (concat (symbol-name function)
 			 (if suffix suffix "-original")))))
@@ -85,6 +150,7 @@ If SUFFIX is nil, \"-original\" is added. "
   "* if this variable is string, toroku-region regard this value as yomigana.")
 
 (defun w32-ime-toroku-region (begin end)
+  "Register words between BEGIN and END to the IME dictionary."
   (interactive "r")
   (let ((string (buffer-substring begin end))
 	(w32-ime-buffer-switch-p nil)
@@ -99,6 +165,7 @@ If SUFFIX is nil, \"-original\" is added. "
 ;; for IME management system.
 
 (defun w32-ime-sync-state (window)
+  "Sync IME and IM buffer state on WINDOW."
   (if w32-ime-buffer-switch-p
       (progn
 	(with-current-buffer (window-buffer window)
@@ -126,12 +193,20 @@ If SUFFIX is nil, \"-original\" is added. "
 	      (activate-input-method "W32-IME")))))))))
 
 (defun w32-ime-set-selected-window-buffer-hook (oldbuf newwin newbuf)
+  "A function called by the IME patch's abnormal hook.
+It is called when the buffer of the selected window is set.
+OLDBUF is the previous buffer.  NEWWIN is the newly selected window.
+NEWBUF is the newly set buffer."
   (w32-ime-sync-state newwin))
 
 (defun w32-ime-select-window-hook (old new)
+  "A function called by the IME patch's abnormal hook.
+It is called when the window is selected.
+OLD is the previous window.  NEW is the newly selected window."
   (w32-ime-sync-state new))
 
 (defun w32-ime-mode-line-update ()
+  "Update IME mode line."
   (if (fboundp 'ime-get-mode)
       (progn
 	(cond
@@ -146,10 +221,10 @@ If SUFFIX is nil, \"-original\" is added. "
 	 (t
 	  (setq w32-ime-mode-line-state-indicator
 		(nth 0 w32-ime-mode-line-state-indicator-list))))
-	(force-mode-line-update))
-    ))
+	(force-mode-line-update))))
 
 (defun w32-ime-init-mode-line-display ()
+  "Initialize IME mode line."
   (unless (member 'w32-ime-mode-line-state-indicator mode-line-format)
     (setq w32-ime-mode-line-format-original
 	  (default-value 'mode-line-format))
@@ -165,7 +240,9 @@ If SUFFIX is nil, \"-original\" is added. "
 				mode-line-format))))
     (force-mode-line-update t)))
 
+;;;###autoload
 (defun w32-ime-initialize ()
+  "Initialize w32-ime.el."
    (when (and (or (eq system-type 'windows-nt) (eq system-type 'cygwin))
 	      (eq window-system 'w32)
 	      (fboundp 'ime-get-mode))
@@ -176,9 +253,9 @@ If SUFFIX is nil, \"-original\" is added. "
      (add-hook 'set-selected-window-buffer-functions
 	       'w32-ime-set-selected-window-buffer-hook)
      (define-key global-map [kanji] 'toggle-input-method)))
-;;     (set-keyboard-coding-system 'utf-8)))
 
 (defun w32-ime-uninitialize ()
+  "Uninitialize w32-ime.el."
   (when (and (or (eq system-type 'windows-nt) (eq system-type 'cygwin))
 	     (eq window-system 'w32)
 	     (fboundp 'ime-get-mode))
@@ -192,12 +269,16 @@ If SUFFIX is nil, \"-original\" is added. "
     (define-key global-map [kanji] 'ignore)))
 
 (defun w32-ime-exit-from-minibuffer ()
+  "A function called from \"minibuffer-exit-hook\"."
   (when w32-ime-buffer-switch-p
     (deactivate-input-method))
   (when (<= (minibuffer-depth) 1)
     (remove-hook 'minibuffer-exit-hook 'w32-ime-exit-from-minibuffer)))
 
 (defun w32-ime-state-switch (&optional arg)
+  "Switch IME state.
+If ARG is omitted or nil, turn off the IME state.
+Otherwise, turn off the IME state."
   (if arg
       (progn
 	(setq deactivate-current-input-method-function
@@ -220,4 +301,20 @@ If SUFFIX is nil, \"-original\" is added. "
 (register-input-method "W32-IME" "Japanese" 'w32-ime-state-switch ""
 		       "W32 System IME")
 
+;;
+;; Windows only functions
+;;
+
+(declare-function ime-get-mode "w32fns.c")
+(declare-function ime-force-on "w32fns.c")
+(declare-function ime-force-off "w32fns.c")
+(declare-function w32-set-ime-mode "w32fns.c")
+(declare-function w32-ime-register-word-dialog "w32fns.c")
+
+;;
+;; provide
+;;
+
 (provide 'w32-ime)
+
+;;; w32-ime.el ends here
